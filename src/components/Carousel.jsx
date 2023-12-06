ce97dc1988e1e91828e4b44b0110495155619ae3
sakshxm08 <saksham2211gambhir@gmail.com>
2023-12-06 21:07:08 +0530

    Removed unnecessary files

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -39,7 +39,7 @@ const Carousel = () => {
     speed: 500,
     slidesToShow: 1,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     slidesToScroll: 1,
     pauseOnHover: true,
   };
