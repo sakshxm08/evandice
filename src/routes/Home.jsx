69a7563ce32dda3c76cb4cb66090d39b3786dae7
sakshxm08 <saksham2211gambhir@gmail.com>
2023-12-07 03:34:43 +0530

    added community data

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -17,6 +17,29 @@ const Home = () => {
         </div>
         <FeaturedCarousel />
       </div>
+      <div className="flex flex-col items-center justify-center py-6 max-w-7xl w-fit mx-auto gap-16">
+        <h1 className="uppercase text-title font-title">
+          join a{" "}
+          <span className=" bg-gradient-to-br from-[#FF4545] from-20% via-[#DF9438] to-[#FBBC05] bg-clip-text text-transparent">
+            community
+          </span>{" "}
+          of thousands
+        </h1>
+        <div className="grid grid-cols-3 w-full">
+          <div className="flex flex-col gap-2">
+            <div className="text-7xl font-medium">11.5M+</div>
+            <div>active accounts</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="text-7xl font-medium">21.9M</div>
+            <div>registrations till date</div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <div className="text-7xl font-medium">&#8377; 4,000</div>
+            <div>avergae cose per ticket</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
