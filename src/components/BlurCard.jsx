ce005ab2befefa388b362fb628c6d287d45245ce
sakshxm08 <saksham2211gambhir@gmail.com>
2024-01-30 18:47:56 +0530

    responsive blur card

--- src/components/BlurCard.jsx
+++ src/components/BlurCard.jsx
@@ -9,7 +9,7 @@ const BlurCard = ({ width = "w-full", event = [] }) => {
   // Component Starts ---------------------------------------------------------------------------------------------------
   return (
     <div
-      className={`${width} max-h-96 border border-gray-700 flex items-center justify-start gap-8 hover:gap-4 transition-all duration-500 flex-col aspect-[2/3] pt-8 pb-20 px-6 relative mx-auto rounded-2xl overflow-hidden group backdrop-blur-lg bg-blurCard`}
+      className={`${width} max-h-96 border border-gray-700 flex items-center justify-center gap-4 hover:gap-2 transition-all duration-500 flex-col aspect-[2/3] pt-8 pb-20 hover:pb-8 min-h-fit px-6 relative mx-auto rounded-2xl overflow-hidden group backdrop-blur-lg bg-blurCard`}
     >
       <img
         src={blur_card_img}
@@ -22,7 +22,7 @@ const BlurCard = ({ width = "w-full", event = [] }) => {
       <div className="text-xs font-light text-gray-400 text-left">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi at aut
       </div>
-      <div className="flex flex-col w-4/5 mx-autp -mb-28 group-hover:-mb-0 transition-all duration-500">
+      <div className="flex flex-col w-4/5 mx-autp -mb-28 group-hover:-mb-0 transition-all duration-500 mt-4">
         <Link
           to={`/events/${event._id}`}
           className="flex capitalize w-full text-sm to-[#8176AF] bg-gradient-to-l from-transparent bg-[#C0B7E8] hover:bg-[#3f3176] items-center justify-center rounded-full py-2 transition-all opacity-0 group-hover:opacity-100 ease-in-out duration-500"
