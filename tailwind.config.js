33e4bd429f9315d4314fbf3ea19bada79fe05506
sakshxm08 <saksham2211gambhir@gmail.com>
2024-02-16 22:56:31 +0530

    fixed minor tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
     "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
   ],
