62bf0ab15e3af2b2cb5ef3e5db241fbeded55354
sakshxm08 <saksham2211gambhir@gmail.com>
2024-02-16 18:44:10 +0530

    updated verify page design

--- src/routes/AddEvent/Verify.jsx
+++ src/routes/AddEvent/Verify.jsx
@@ -8,26 +8,12 @@ export const Verify = () => {
     <div>
       <Link
         to={-1}
-        className="absolute left-10 top-40 flex font-bold hover:text-yellow transition-all cursor-pointer justify-center items-center gap-2 text-xl"
+        className="absolute left-10 top-36 flex font-bold hover:text-yellow transition-all cursor-pointer justify-center items-center gap-2 text-xl"
       >
         <IoIosArrowRoundBack size={40} />
         Back
       </Link>
-      <div className="max-w-lg mx-auto w-full flex flex-col gap-8 justify-center items-center">
-        <div className="flex flex-col gap-2 w-full">
-          <label htmlFor="map_link" className="text-yellow text-xs">
-            Enter the unique Evandize code for your fest
-          </label>
-          <input
-            type="text"
-            className="py-2 px-4 rounded bg-transparent border disabled:border-gray-600 disabled:text-gray-500 outline-none focus-visible:border-yellow"
-            name="map_link"
-            id="map_link"
-          />
-        </div>
-        <Link className="mx-auto py-3 px-32 text-black bg-yellow hover:bg-yellow/90 transition-all font-bold text-base rounded-md">
-          Go Ahead
-        </Link>
+      <div className="max-w-lg mx-auto w-screen flex flex-col gap-8 justify-center items-center">
         <div className="flex flex-col gap-2 w-full">
           <label htmlFor="map_link" className="text-yellow text-xs">
             Add Email IDs to send this code
@@ -39,8 +25,22 @@ export const Verify = () => {
             id="map_link"
           />
         </div>
-        <Link className="mx-auto py-3 px-32 text-black bg-green-700 hover:bg-green-800 transition-all font-bold text-base rounded-md">
+        <Link className="mx-auto py-3 px-16 text-center max-w-xs w-full text-black bg-green-700 hover:bg-green-800 transition-all font-bold text-base rounded-md">
           Send Code
+        </Link>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="map_link" className="text-yellow text-xs">
+            Enter the unique Evandize code for your fest
+          </label>
+          <input
+            type="text"
+            className="py-2 px-4 rounded bg-transparent border disabled:border-gray-600 disabled:text-gray-500 outline-none focus-visible:border-yellow"
+            name="map_link"
+            id="map_link"
+          />
+        </div>
+        <Link className="mx-auto py-3 px-16 text-center max-w-xs w-full text-black bg-yellow hover:bg-yellow/80 transition-all font-bold text-base rounded-md">
+          Go Ahead
         </Link>
       </div>
     </div>
